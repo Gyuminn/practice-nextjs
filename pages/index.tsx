de440fd6efcc6e0c8bef59a5ed01d5dc644da45a
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-21 20:38:55 +0900

    practice tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,86 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-slate-400 py-20 px-10 grid gap-5 min-h-screen">
+      <div className="bg-white p-6 rounded-3xl shadow-lg">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Tooly Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+          Cehckout
+        </div>
+      </div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-lg">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">$340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full"></div>
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Gyuminn</span>
+            <span className="text-sm text-gray-400">한국</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-lg">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅</span>
+          <div className="space-x-3">
+            <span>🔥 4.9</span>
+            <span className="shadow-xl p-2 rounded-md">💜</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium mb-1.5 text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-3 flex justify-between items-center">
+            <div>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-3">
+              <button className="p-1.5 rounded-xl bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="p-1.5 rounded-xl bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl mb-3">$450</span>
+            <button className="bg-blue-500 py-2 px-5 text-center text-xs text-white rounded-lg">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
